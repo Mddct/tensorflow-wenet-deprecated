6a8fd1bf83e50da78f24e92fc081096f218bdaf1
Mddct <hamddct@gmail.com>
2022-09-22 18:29:27 +0800

    Update activations.py

--- draft/activations.py
+++ draft/activations.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import math
 
 # Supported activation functions.
 _ACTIVATIONS = {
@@ -18,7 +19,7 @@ _ACTIVATIONS = {
         lambda x: tf.nn.gelu(x, approximate=True),
     'GELU_RAW':
         lambda x: 0.5 * x * (  # pylint: disable=g-long-lambda
-            1 + tf.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * tf.pow(x, 3)))),
+            1 + tf.tanh(tf.math.sqrt(2 / math.pi) * (x + 0.044715 * tf.pow(x, 3)))),
     'SWISH':
         tf.nn.swish,
     'SOFTPLUS':
