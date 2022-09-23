ce63e8a2b98ef7e4d5a8501239be00215bbd1dfb
Mddct <hamddct@gmail.com>
2022-09-23 13:27:36 +0800

    Update attention.py

--- draft/attention.py
+++ draft/attention.py
@@ -1,22 +1,22 @@
 """Multi-Head Attention layer definition."""
-
+### WIP
 import math
 from typing import Tuple
 
 import tensorflow as tf
 
 
-def XavierUniform(shape, dtype):
+def XavierUniform(shape, dtype, scale=1.0):
     method = 'xavier'
     """Xavier initialization (x = sqrt(6. / (in + out)); scale*[-x, x])."""
     if not shape:
       raise ValueError('\'shape\' must not be \'None\' or 0 for XavierUniform')
-    fan_in, fan_out = GetFanInFanOut(shape, combined_layers_dims)
+    # fan_in, fan_out = GetFanInFanOut(shape, combined_layers_dims)
     if method == 'xavier':
       limit = math.sqrt(6. / (fan_in + fan_out))
-    elif method == 'geo_mean_xavier':
-      limit = math.sqrt(3. / math.sqrt(fan_in * fan_out))
-    return scale * tf.random.uniform(shape, -limit, limit, dtype, seed)
+    # elif method == 'geo_mean_xavier':
+    #   limit = math.sqrt(3. / math.sqrt(fan_in * fan_out))
+    return scale * tf.random.uniform(shape, -limit, limit, dtype)
 
 class MultiHeadedAttention(tf.keras.layers.Layer):
     """Multi-Head Attention layer.
@@ -32,11 +32,13 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
         # We assume d_v always equals d_k
         self.d_k = n_feat // n_head
         self.h = n_head
+        self.n_feat =  n_feat
+
         self.linear_q = tf.keras.layers.Dense(n_feat)
         self.linear_k = tf.keras.layers.Dense(n_feat)
         self.linear_v =tf.keras.layers.Dense(n_feat)
         self.linear_out = tf.keras.layers.Dense(n_feat)
-        self.dropout = tf.keras.layers.Dropout(p=dropout_rate)
+        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
         
     def forward_qkv(
         self, query: tf.Tensor, key: tf.Tensor, value: tf.Tensor
@@ -67,9 +69,8 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
 
     def forward_attention(
         self, value: tf.Tensor, scores: tf.Tensor,
-        mask: tf.Tensor = tf.ones((0, 0, 0), dtype=tf.bool, 
-        training: bool = true)
-    ) -> tf.Tensor:
+        mask: tf.Tensor = None,
+        training: bool = True) -> tf.Tensor:
         """Compute attention context vector.
         Args:
             value (tf.Tensor): Transformed value, size
@@ -92,16 +93,14 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
         x = tf.matmul(p_attn, value)  # (batch, head, time1, d_k)
         x = tf.reshape(tf.transpose(x, [0,1,3,2]), 
                        [n_batch, -1, self.h * self.d_k]) # (batch, time1, d_model)
-
         return self.linear_out(x)  # (batch, time1, d_model)
 
     def call(self, query: tf.Tensor, key: tf.Tensor,
                 value: tf.Tensor,
-                mask: tf.Tensor = tf.ones((0, 0, 0), tf=tf.bool),
-                pos_emb: tf.Tensor = tf.empty(0),
-                cache: tf.Tensor = tf.zeros((0, 0, 0, 0)),
-                training: bool = true,
-                ) -> Tuple[tf.Tensor, tf.Tensor]:
+                mask: tf.Tensor = None,
+                pos_emb: tf.Tensor = None,
+                cache: tf.Tensor = None,
+                training: bool = True) -> Tuple[tf.Tensor, tf.Tensor]:
         """Compute scaled dot product attention.
         Args:
             query (tf.Tensor): Query tensor (#batch, time1, size).
@@ -143,6 +142,17 @@ class MultiHeadedAttention(tf.keras.layers.Layer):
         return self.forward_attention(v, scores, mask), new_cache
 
 
+    def get_config(self):
+        conf = super(MultiHeadedAttention, self).get_config()
+        conf.update({"head": self.h})
+        conf.update({"feats": self.n_feat})
+        conf.update({"linear_q": self.linear_q.get_config()})
+        conf.update({"linear_k": self.linear_q.get_config()})
+        conf.update({"linear_v": self.linear_q.get_config()})
+        conf.update({"dropout": self.dropout.get_config()})
+           
+        return conf
+        
 class RelPositionMultiHeadedAttention(MultiHeadedAttention):
     """Multi-Head Attention layer with relative position encoding.
     Paper: https://arxiv.org/abs/1901.02860
@@ -158,13 +168,7 @@ class RelPositionMultiHeadedAttention(MultiHeadedAttention):
         self.linear_pos = tf.keras.layers.Dense(n_feat)
 
         
-   def build(self, input_shape):  # Create the state of the layer (weights)
-            # these two learnable bias are used in matrix c and matrix d
-        # as described in https://arxiv.org/abs/1901.02860 Section 3.3
-#         self.pos_bias_u = nn.Parameter(tf.Tensor(self.h, self.d_k))
-#         self.pos_bias_v = nn.Parameter(tf.Tensor(self.h, self.d_k))
-#         tf.nn.init.xavier_uniform_(self.pos_bias_u)
-#         tf.nn.init.xavier_uniform_(self.pos_bias_v)
+    def build(self, input_shape):
         pos_bis_u_init_ = tf.random_normal_initializer()
         self.pos_bias_u = tf.Variable(
           initial_value=XavierUniform(
@@ -180,25 +184,25 @@ class RelPositionMultiHeadedAttention(MultiHeadedAttention):
           trainable=True,
         )
         
-    def rel_shift(self, x, zero_triu: bool = False):
-      x_size = tf.shape(x)
+    def rel_shift_(self, x, zero_triu: bool = False):
+        x_size = tf.shape(x)
 
-      x = tf.pad(x, [[0, 0], [1, 0], [0, 0], [0, 0]])
-      x = tf.reshape(x, [x_size[1] + 1, x_size[0], x_size[2], x_size[3]])
-      x = tf.slice(x, [1, 0, 0, 0], [-1, -1, -1, -1])
-      x = tf.reshape(x, x_size)
-      if zero_triu:
-            x = tf.linalg.band_part(ones, -1, x.size(3) - x.size(2))
-       
-      return x
+        x = tf.pad(x, [[0, 0], [1, 0], [0, 0], [0, 0]])
+        x = tf.reshape(x, [x_size[1] + 1, x_size[0], x_size[2], x_size[3]])
+        x = tf.slice(x, [1, 0, 0, 0], [-1, -1, -1, -1])
+        x = tf.reshape(x, x_size)
+        if zero_triu:
+                x = tf.linalg.band_part(ones, -1, x.size(3) - x.size(2))
+        
+        return x
  
 
     def call(self, query: tf.Tensor,
                 key: tf.Tensor, value: tf.Tensor,
-                mask: tf.Tensor = tf.ones((0, 0, 0), dtype=tf.bool),
-                pos_emb: tf.Tensor = tf.empty(0),
-                cache: tf.Tensor = tf.zeros((0, 0, 0, 0)),
-                trainint: bool = True,
+                mask: tf.Tensor = None,
+                pos_emb: tf.Tensor = None,
+                cache: tf.Tensor = None,
+                training: bool = True,
                 ) -> Tuple[tf.Tensor, tf.Tensor]:
         """Compute 'Scaled Dot Product Attention' with rel. positional encoding.
         Args:
@@ -242,17 +246,16 @@ class RelPositionMultiHeadedAttention(MultiHeadedAttention):
         # first compute matrix a and matrix c
         # as described in https://arxiv.org/abs/1901.02860 Section 3.3
         # (batch, head, time1, time2)
-        matrix_ac = tf.matmul(q_with_bias_u, tf.transpose(k, [0,1,3,2])
+        matrix_ac = tf.matmul(q_with_bias_u, tf.transpose(k, [0,1,3,2]))
 
         # compute matrix b and matrix d
         # (batch, head, time1, time2)
-        matrix_bd = tf.matmul(q_with_bias_v, tf.transpose(p, [0,1,3,2])
+        matrix_bd = tf.matmul(q_with_bias_v, tf.transpose(p, [0,1,3,2]))
         # Remove rel_shift since it is useless in speech recognition,
         # and it requires special attention for streaming.
-        # matrix_bd = self.rel_shift(matrix_bd)
+        # matrix_bd = self.rel_shift_(matrix_bd)
 
         scores = (matrix_ac + matrix_bd) / math.sqrt(
             self.d_k)  # (batch, head, time1, time2)
 
-        return self.forward_attention(v, scores, mask), 
-
+        return self.forward_attention(v, scores, mask), new_cache
