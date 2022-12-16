fix ctc gradient

draft/wenet/dataset/dataset.py
@@ -22,63 +22,69 @@ def look_up_table(symbol_table_path, unk="<unk>"):
 def Dataset(
     conf,
     symbol_table_path,
-    data_list_file,
+    file_pattern,
     global_batch_size=1,
-    prefetch=tf.data.AUTOTUNE,
-    data_type="shard",
     strategy=None,
+    prefetch=1000,
     training: bool = True,
     cache: bool = False,
+    max_io_parallelism: int = 256,
     tf_data_service: Optional[str] = None,
 ):
 
     symbol_table, vocab_size = look_up_table(symbol_table_path)
 
-    def dataset_fn(input_context=None):
-        dataset = tf.data.TextLineDataset(data_list_file)
-        if data_type == 'shard':
-            # eacho shard: dataset element ["shard1.txt", 'shard2.txt'....]
-            # asynchronously parallel read multiple shard
-            dataset = dataset.interleave(
-                lambda elem: tf.data.TextLineDataset(elem),
-                cycle_length=prefetch,
-                num_parallel_calls=prefetch,
-            )
-            dataset = dataset.apply(tf.data.experimental.ignore_errors())
+    def _load_data_list(filename):
+        return tf.data.TextLineDataset(filename)
 
+    def dataset_fn(input_context=None):
+        if training:
+            dataset = tf.data.Dataset.list_files(file_pattern, shuffle=True)
+        else:
+            dataset = tf.data.Dataset.list_files(file_pattern, shuffle=False)
+            # shard across multi workers
         if strategy is not None and input_context is not None:
             dataset = dataset.shard(input_context.num_input_pipelines,
                                     input_context.input_pipeline_id)
+
+        # Read files and interleave results. When training, the order of the examples
+        # will be non-deterministic.
+        options = tf.data.Options()
+        if training:
+            options.deterministic = False
+        else:
+            options.deterministic = True
+
+        dataset = dataset.interleave(
+            _load_data_list,
+            cycle_length=max_io_parallelism,
+            num_parallel_calls=tf.data.AUTOTUNE).with_options(options)
+
         shuffle = conf.get('shuffle', True)
         if shuffle:
             shuffle_conf = conf.get('shuffle_conf', {})
             dataset = dataset.shuffle(buffer_size=shuffle_conf['shuffle_size'],
                                       reshuffle_each_iteration=True)
-        if training and cache is not None:
+        if training:
+            # for training , repeat forwever, until reach max steps
             dataset = dataset.repeat()
-            shard = 1000
-            prefetch_wav = 100
-            dataset = dataset.window(shard)
-
+            # interleave wav read
             dataset = dataset.interleave(
-                lambda window: window.map(lambda line: processor.parse_line(
-                    line, symbol_table),
-                                          num_parallel_calls=tf.data.AUTOTUNE).
-                apply(tf.data.experimental.ignore_errors()),
-                num_parallel_calls=tf.data.AUTOTUNE,
-                block_length=prefetch_wav,
+                lambda elem: tf.data.Dataset.from_tensors(elem).map(
+                    lambda line: processor.parse_line(line, symbol_table),
+                    num_parallel_calls=1).ignore_errors(),
+                cycle_length=prefetch,
+                num_parallel_calls=max_io_parallelism,
             )
 
         if cache:
             # for small dataset we can cache all raw wav in memory
             dataset = dataset.cache()
 
-        # dataset = dataset.map(
-        #     lambda line: processor.parse_line(line, symbol_table),
-        #     num_parallel_calls=tf.data.AUTOTUNE)
-        # file may not found in  parse_line, ignore error
-        # dataset = dataset.apply(tf.data.experimental.ignore_errors())
-
+        # 1 one sample, like: resample speed 、spec sub、 spec trim、filter ...
+        # 2 fbank
+        # 3 group by window
+        # 5 batch sample, like: spec aug
         speed_perturb = conf.get('speed_perturb', False)
         if speed_perturb:
             dataset = dataset.map(
@@ -113,34 +119,20 @@ def Dataset(
         if strategy is not None:
             batch_size = input_context.get_per_replica_batch_size(
                 global_batch_size)
-        # bucket
-        # TODO: from config
-        bucket_boundaries = [500, 10 * 100, 15 * 100,
-                             20 * 100]  # [0-5s) [5s, 10s) [10s,15) ...
+            # bucket
+            # TODO: from config
+            # [0-1s)... [5s, 10s) [10s,15) ...
+        bucket_boundaries = [5 * 100, 10 * 100, 15 * 100, 20 * 100]
         bucket_batch_sizes = [batch_size] * (len(bucket_boundaries) + 1)
+        # TODO: replace with gorup by window, each batch has similar total tokens
         dataset = dataset.bucket_by_sequence_length(
-            lambda inputs: tf.cast(inputs[1], tf.int32),
-            bucket_boundaries,
-            bucket_batch_sizes,
-            pad_to_bucket_boundary=False,
-            drop_remainder=True,
+            lambda feats, feats_lens, labels, labels_lens: feats_lens,
+            bucket_boundaries=bucket_boundaries,
+            bucket_batch_sizes=bucket_batch_sizes,
             padded_shapes=([None, None], [], [None], []),
-            # TODO: ignore id not 0 for text padding id
             padding_values=(0.0, None, tf.cast(0, dtype=tf.int32), None),
-        )
-        # group by window
-        if input_context is not None:
-            window_size = input_context.num_replicas_in_sync
-            dataset = dataset.group_by_window(
-                key_func=lambda inputs: tf.cast(  # pylint: disable=g-long-lambda
-                    tf.shape(inputs[0])[1], tf.int64),
-                reduce_func=lambda inputs:
-                (tf.data.Dataset.from_tensors(inputs[0]),
-                 tf.data.Dataset.from_tensors(inputs[1]),
-                 tf.data.Dataset.from_tensors(inputs[2]),
-                 tf.data.Dataset.from_tensors(inputs[3])),
-                window_size=window_size)
-            dataset = dataset.flat_map(lambda x: x)
+            drop_remainder=True)
+        dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
         # dataset = dataset.padded_batch(batch_size=batch_size,
         #                                padded_shapes=([None,
@@ -164,7 +156,7 @@ def Dataset(
                 lambda feats, feats_length, labels, labels_length:
                 (processor.spec_aug(feats, feats_length, augmenter),
                  feats_length, labels, labels_length), tf.data.AUTOTUNE)
-        dataset = dataset.prefetch(tf.data.AUTOTUNE)
+            dataset = dataset.prefetch(tf.data.AUTOTUNE)
 
         if tf_data_service is not None:
             if not hasattr(tf.data.experimental, 'service'):

draft/wenet/dataset/processor.py
@@ -59,14 +59,10 @@ def spec_trim(feats, max_t=20):
                    lambda: feats[:(max_frames - length), :], lambda: feats)
 
 
-@tf.function
-def spec_aug(feats, feats_length, augmenter=None):
-    if augmenter is None:
-        return feats
+def spec_aug(feats, feats_length, augmenter):
     return augmenter(feats, feats_length)
 
 
-@tf.function
 def filter(waveform,
            sr,
            labels,
@@ -79,23 +75,20 @@ def filter(waveform,
     num_frames = tf.shape(waveform)[0] / sr * 100
     toks_length = tf.shape(labels)[0]
 
-    if num_frames < min_length:
-        return False
-    if num_frames > max_length:
-        return False
-    if toks_length < token_min_length:
-        return False
-    if toks_length > token_max_length:
-        return False
-    if num_frames != 0:
-        frames_per_tok = tf.cast(toks_length, dtype=tf.float32) / tf.cast(
-            num_frames, dtype=tf.float32)
-        # frames_per_tok = tf.cast(toks_length, dtype=tf.float32) / tf.cast(
-        # num_frames, dtype=tf.float32)
-        if frames_per_tok < min_output_input_ratio:
-            return False
-        if frames_per_tok > max_output_input_ratio:
-            return False
-        return True
-    else:
-        return False
+    frames_cond = tf.logical_and(
+        num_frames <= max_length,
+        num_frames >= min_length,
+    )
+    tokens_cond = tf.logical_and(
+        toks_length <= token_max_length,
+        toks_length >= token_min_length,
+    )
+
+    frames_per_tok = tf.cast(toks_length, dtype=tf.float32) / tf.cast(
+        num_frames, dtype=tf.float32)
+    frames_per_tok_cond = tf.logical_and(
+        frames_per_tok >= min_output_input_ratio,
+        frames_per_tok <= max_output_input_ratio,
+    )
+    return tf.logical_and(tf.logical_and(frames_cond, tokens_cond),
+                          frames_per_tok_cond)

draft/wenet/transformer/asr_model.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from keras.backend import update
 import tensorflow as tf
 from wenet.transformer.ctc import CTCDense
 from wenet.transformer.decoder import TransformerDecoder
@@ -78,10 +79,27 @@ class ASRModel(tf.keras.Model):
         if self.ctc_weight != 1.0:
             decoder_out, ys_out_pad, r_decoder_out, r_ys_out_pad = self.forward_decoder(
                 encoder_out, encoder_mask, text, text_lengths)
-            encoder_out = self.ctc_dense(encoder_out, training=True)
         else:
             decoder_out, ys_out_pad, r_decoder_out, r_ys_out_pad = None, None, None, None
 
+        if self.ctc_weight != 0.0:
+            # NOTE: we will use sparse tensor cuda ctc
+            # but cuda ctc doesn't zero out gradients when exceeds the seq_lens
+            # which makes training wrong, so turn value to zero
+            # https://github.com/tensorflow/tensorflow/issues/41280
+            encoder_out = self.ctc_dense(encoder_out, training=True)
+            encoder_out_before = encoder_out
+
+            @tf.custom_gradient
+            def ctc_zero_out(input):
+
+                def grad(upstream):
+                    return upstream * tf.transpose(encoder_mask, [0, 2, 1])
+
+                return tf.identity(input), grad
+
+            encoder_out = ctc_zero_out(encoder_out_before)
+            # encoder_out = encoder_out * tf.transpose(encoder_mask, [0, 2, 1])
         return (
             encoder_out,
             encoder_out_lens,
@@ -116,6 +134,7 @@ class ASRModel(tf.keras.Model):
 
         return decoder_out, ys_out_pad, r_decoder_out, r_ys_out_pad
 
+    @tf.function
     def compute_loss(
         self,
         encoder_logits,
@@ -129,7 +148,7 @@ class ASRModel(tf.keras.Model):
     ):
         loss_ctc = None
         # TODO: move label convert to data pipeline
-        # NOTE: sparse label, tf will chose cuda ctc which faster than dense even on gpu
+        # NOTE: sparse label, tf will choose cuda ctc which faster than dense even on gpu
         if self.ctc_weight != 0.0:
             labels_sparse = tf.sparse.from_dense(encoder_labels)
             loss_ctc = tf.nn.ctc_loss(
@@ -138,6 +157,7 @@ class ASRModel(tf.keras.Model):
                 None,
                 encoder_logits_lens,
                 logits_time_major=False,
+                blank_index=0,
             )
         loss_att = None
         if self.ctc_weight != 1.0:

draft/wenet/transformer/encoder.py
@@ -162,7 +162,8 @@ class BaseEncoder(tf.keras.layers.Layer):
             xs, masks, self.use_dynamic_chunk, self.use_dynamic_left_chunk,
             decoding_chunk_size, self.static_chunk_size,
             num_decoding_left_chunks)
-        chunk_attention_bias = get_encoder_attention_bias(chunk_attention_bias)
+        chunk_attention_bias = get_encoder_attention_bias(
+            chunk_attention_bias)  # [B, 1, *, T]
 
         for layer in self.encoders:
             xs = layer(xs,

draft/wenet/utils/executor.py
@@ -21,7 +21,6 @@ class AsrTrainer(orbit.StandardTrainer):
         with self.strategy.scope():
             self.optimizer = optimizer
             self.model = model
-            self.global_step = self.optimizer.iterations
 
         self.global_batch_size = global_batch_size
 
@@ -44,12 +43,11 @@ class AsrTrainer(orbit.StandardTrainer):
     def train_step(self, iterator):
 
         def step_fn(inputs):
-
+            _, _, labels, labels_length = inputs
             with tf.GradientTape() as tape:
                 # feats, feats_length, labels, labels_length = inputs
                 # labels = tf.cast(labels, dtype=tf.int32)
                 # labels_length = tf.cast(labels_length, dtype=tf.int32)
-                _, _, labels, labels_length = inputs
                 encoder_out, encoder_out_lens, decoder_out, ys_out_pad, r_decoder_out, r_ys_out_pad = self.model(
                     inputs)
                 loss_dict = self.model.compute_loss(